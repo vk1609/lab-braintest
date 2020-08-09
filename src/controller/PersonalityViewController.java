fa701fbb5d078cf1bdcef2f6c358397d0c64fa5d
unknown <vamsi3353@gmail.com>
2020-08-09 21:16:14 +0530

    vamsikrishna

--- src/controller/PersonalityViewController.java
+++ src/controller/PersonalityViewController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.PersonalityCalculator;
+
 
 @WebServlet(urlPatterns= {"/personality"})
 public class PersonalityViewController extends HttpServlet {
@@ -33,8 +35,14 @@ public class PersonalityViewController extends HttpServlet {
 		String options=request.getParameter("selectedOptions");
 		
 		System.out.println(options);
+		
+        PersonalityCalculator pc=new PersonalityCalculator();
+		
+		// create an object for the PersonalityCalculator
+		String message= pc.findYourBrainType(options);
+
 				
-		/* 
+	
 
 		request.setAttribute("message", message);
 		
@@ -53,7 +61,7 @@ public class PersonalityViewController extends HttpServlet {
 				rd.forward(request, response);
 			}
 		
-		}*/
+		}
 		}
 	}
 
--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -1,2 +1,44 @@
 package service;
 
+public class PersonalityCalculator{
+	@SuppressWarnings("unused")
+	public int findAnswer(String options) {
+//		split the option and store in the separate string
+		String sc[] = options.split(",");
+		int n = sc.length;
+		int arr[] = new int[n];
+		for(int i=0; i<n ; i++) {
+			//parse int convert the string into the integer.
+			//change the type.
+			arr[i] = Integer.parseInt(sc[i]);
+			return arr[i];
+			
+		}
+
+		 int x=arr[0]+arr[1]+arr[2]+arr[4]+arr[7]+arr[9]+arr[10]+arr[11]+arr[13]+arr[17]+arr[19];
+		 System.out.println(x);
+		 int y=arr[3]+arr[5]+arr[6]+arr[12]+arr[14]+arr[15]+arr[16]+arr[18];
+		 System.out.println(y);
+		 int Z=66-x+y;
+		 return Z;
+	}
+
+
+public String findYourBrainType(String options) {
+	
+	 int Z=findAnswer(options);
+	        
+	 if((Z>=20)&&(Z<=25)) {
+		 return "Left-Brained";
+	 }
+	 else if((Z>=56)&&(Z<=64)) {
+		 return "No clear preference ";
+	 }
+	 else if((Z>=65)&&(Z<=100)) {
+	    return "Right-brained";
+	 }
+	 else 
+		 return "something went wrong";
+	
+}
+}
